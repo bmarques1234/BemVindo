4b3f38c5465603db38bd49bffb46aebf0fda9fab
Bruno Augusto Klein <bruno.klein@digitaldesk.com.br>
2016-04-20 16:03:39 -0300

    pag principal errada

--- animation.js
+++ animation.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+<<<<<<< 116408a9c3df6eff80a8fecf02654eb85cc2112d
 <<<<<<< 1012bfc2cf66d9fe2fb001a5b5280342cd71003d
 	$('#imagem').hover(function(){
 		$("#body").slideToogle("slow");
@@ -7,11 +8,18 @@ $(document).ready(function(){
 		$("#body").slideUp();
 =======
 	$('#foto').hide();
+=======
+>>>>>>> pag principal errada
 	$('#imagem').hover(function(){
-		$('#foto').fadeIn("slow");
+		$("#body").slideToogle("slow");
 	});
+<<<<<<< 116408a9c3df6eff80a8fecf02654eb85cc2112d
 	$('#imagem').mouseleave(function(){
 		$('#foto').fadeOut("fast");
 >>>>>>> animação
+=======
+	$("imagem").mouseleave({
+		$("#body").slideUp();
+>>>>>>> pag principal errada
 	});
 });
