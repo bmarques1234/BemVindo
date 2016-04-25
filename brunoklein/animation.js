67e379ae2b8a4d5b5969aa9309bf5f6d47e66e5f
Bruno Augusto Klein <bruno.klein@digitaldesk.com.br>
2016-04-25 16:29:55 -0300

    Animação

---
+++ brunoklein/animation.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$(".conteudo").hide();
+	$(".conteudo").slideUp();
+    $("#animacaopessoais").click(function(){
+        $("#animacaopessoais>.conteudo").stop().slideToggle();
+    });
+	
+	 $("#animacaoprofissionais").click(function(){
+        $("#animacaoprofissionais>.conteudo").stop().slideToggle();
+    });
+	
+	 $("#animacaoadicionais").click(function(){
+        $("#animacaoadicionais>.conteudo").stop().slideToggle();
+    });
+});
+
