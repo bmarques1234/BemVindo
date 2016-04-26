d765f428863366bfeef4a3f1a93a7e1f6c51a1d4
Luísa Antoni da Rocha <luisa.rocha@digitaldesk.com.br>
2016-04-26 16:29:13 -0300

    sem barra de pesquisar

--- luisa/animacao.js
+++ luisa/animacao.js
@@ -1,3 +1,4 @@
+<<<<<<< eb96da950cd3075a457418aa5ffe59bd73976dab
 <<<<<<< e225190de4f5e11d5b9174a862ab600f59ace022
 <<<<<<< 3e850582b412b1a06427963d20360a9a6de6f56f
 <<<<<<< 902bb323ebaf9599b7c0b8c939d2f72d04d24c84
@@ -45,13 +46,19 @@ $(document).ready(function(){
 >>>>>>> algumas alterações
 window.alert("Seja bem vindo(a).");
 	
+=======
+>>>>>>> sem barra de pesquisar
 $(document).ready(function(){
-		$(".fonteitalico").slideUp();
-		$("#flip").click(function(){
-			$("#flip>.fonteitalico").stop().slideToggle("slow");
-		});
+	
+	$(".fonteitalico").slideUp();
+	$("#flip").click(function(){
+		$("#flip>.fonteitalico").stop().slideToggle("slow");
 	});
+<<<<<<< eb96da950cd3075a457418aa5ffe59bd73976dab
 <<<<<<< e225190de4f5e11d5b9174a862ab600f59ace022
 >>>>>>> com icone e animacao certo
 =======
 >>>>>>> algumas alterações
+=======
+});
+>>>>>>> sem barra de pesquisar
