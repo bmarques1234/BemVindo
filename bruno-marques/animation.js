5534d03a0d82643535d6bbd1907e0b660e28ded1
Bruno Agostini Marques <bruno.marques@digitaldesk.com.br>
2016-04-26 11:16:31 -0300

    botoes play

--- bruno-marques/animation.js
+++ bruno-marques/animation.js
@@ -35,8 +35,10 @@ $(document).ready(function(){
 >>>>>>> ajuste videos
 	$("#pauseSoja").hide();
 	$("#pauseManeva").hide();
+<<<<<<< 15ea8ce934e1bb78b823e5fae6181f87266d9f49
 	$("#showBorder").hide();
 	$("#showDivinity").hide();
+<<<<<<< 03b3ebae205893be2b4a84863d9d69956ea5264a
 <<<<<<< 02eeaad02688f25b3047bfe077efef040ab0c085
 =======
 >>>>>>> ajuste videos
@@ -75,6 +77,10 @@ $(document).ready(function(){
 >>>>>>> pull
 =======
 >>>>>>> ajuste videos
+=======
+=======
+>>>>>>> botoes play
+>>>>>>> botoes play
 	$("#personal").click(function(){
 		$("#personalTitle>.content").stop().slideToggle("slow");
 	});
@@ -101,6 +107,7 @@ $(document).ready(function(){
 		videoSoja.load();
 		$("#pauseSoja").hide();
 		$("#playSoja").show();
+<<<<<<< e777a6e1fff567e11310c6d89a3746abd9397cb3
 <<<<<<< b5ef8a5199984e4b204348c65e04b1c51791ce19
 <<<<<<< f247abb63056a4b1a13f6bfa5cecdb4432522054
 	});
@@ -141,6 +148,9 @@ $(document).ready(function(){
 	});
 =======
 =======
+=======
+<<<<<<< 03b3ebae205893be2b4a84863d9d69956ea5264a
+>>>>>>> botoes play
 <<<<<<< c228c476b3280e43e2cbe534cef6fbdf98445857
 >>>>>>> botoes play
 	});
@@ -188,6 +198,9 @@ $(document).ready(function(){
 >>>>>>> botao video e audio
 =======
 =======
+=======
+<<<<<<< 15ea8ce934e1bb78b823e5fae6181f87266d9f49
+>>>>>>> botoes play
 	});
 	$("#playManeva").click(function(){
 		videoManeva.play();
@@ -224,6 +237,7 @@ $(document).ready(function(){
 		$("#hideDivinity").show();
 		$("#showDivinity").hide();
 	});
+<<<<<<< 03b3ebae205893be2b4a84863d9d69956ea5264a
 <<<<<<< 5f4b5e7dc3274e61ab707152404cd72e096cc79a
 	var videoSoja=document.getElementById("soja");
 	var videoManeva=document.getElementById("maneva");
@@ -234,4 +248,34 @@ $(document).ready(function(){
 =======
 >>>>>>> botao video e audio
 >>>>>>> botao video e audio
+=======
+=======
+	});
+	$("#playManeva").click(function(){
+		videoManeva.play();
+		$("#playManeva").hide();
+		$("#pauseManeva").show();
+	});
+	$("#pauseManeva").click(function(){
+		videoManeva.pause();
+		$("#pauseManeva").hide();
+		$("#playManeva").show();
+	});
+	$("#replayManeva").click(function(){
+		videoManeva.load();
+		$("#pauseManeva").hide();
+		$("#playManeva").show();
+	});
+	$("#clickBorder").click(function(){
+		$("#border").fadeToggle();
+		
+	});
+	$("#clickDivinit").click(function(){
+		$("#divinit").fadeToggle();
+		
+	});
+	var videoSoja=document.getElementById("soja");
+	var videoManeva=document.getElementById("maneva");
+>>>>>>> botoes play
+>>>>>>> botoes play
 });
