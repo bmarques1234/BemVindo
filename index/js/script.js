09ccc3e7e6cbde36b4d59e36b9d14df8b4ad0d0c
Geison Camara <geison.camara@digitaldesk.com.br>
2016-04-26 16:11:28 -0300

    #56

--- index/js/script.js
+++ index/js/script.js
@@ -13,29 +13,6 @@ $(document).ready(function(){
 		});
 	}
 	
-	function ocultar(){
-		$('.close').stop().animate({top: '-20px'}),100;
-	}
-
-	$('.open').on("click", function(){
-		$('.close').fadeIn(100);
-		$('#imagem > div.mensagem').css('display', 'none');
-		$('#imagem > div.mensagem').stop().animate({top: '100%'}),100;
-		$('#imagem > div.fundo').stop().fadeIn(100);
-		$('#imagem > div.nomes').stop().fadeIn(100);
-	});
-	
-	$('.close').on("click", function(){
-		$('#imagem > div.nomes').stop().fadeOut(100);
-		$('#imagem > div.fundo').stop().fadeOut(100);
-		$('#imagem > div.mensagem').stop().fadeOut(100);
-		$('#imagem > div.mensagem').css('display', 'none');
-		$('#imagem > div.mensagem').stop().animate({top: '100%'}),100;
-		$('.close').fadeOut(100);
-		subir();
-		
-	});
-	
 	subir();
 	descer();
 	
