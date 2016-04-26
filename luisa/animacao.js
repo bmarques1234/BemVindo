eb96da950cd3075a457418aa5ffe59bd73976dab
Luísa Antoni da Rocha <luisa.rocha@digitaldesk.com.br>
2016-04-26 13:18:59 -0300

    algumas alterações

--- luisa/animacao.js
+++ luisa/animacao.js
@@ -1,3 +1,4 @@
+<<<<<<< e225190de4f5e11d5b9174a862ab600f59ace022
 <<<<<<< 3e850582b412b1a06427963d20360a9a6de6f56f
 <<<<<<< 902bb323ebaf9599b7c0b8c939d2f72d04d24c84
 <<<<<<< 204a004f8f92889d594f6d864591d7d5a1a6f4b2
@@ -40,6 +41,8 @@ $(document).ready(function(){
 =======
 	inicio();}*/
 	
+=======
+>>>>>>> algumas alterações
 window.alert("Seja bem vindo(a).");
 	
 $(document).ready(function(){
@@ -48,4 +51,7 @@ $(document).ready(function(){
 			$("#flip>.fonteitalico").stop().slideToggle("slow");
 		});
 	});
+<<<<<<< e225190de4f5e11d5b9174a862ab600f59ace022
 >>>>>>> com icone e animacao certo
+=======
+>>>>>>> algumas alterações
