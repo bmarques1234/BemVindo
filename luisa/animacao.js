ec41a5448ce40446d1391d8b479af91b1a54fd91
Luísa Antoni da Rocha <luisa.rocha@digitaldesk.com.br>
2016-04-26 16:47:29 -0300

    sem barra pesquisar

--- luisa/animacao.js
+++ luisa/animacao.js
@@ -1,3 +1,4 @@
+<<<<<<< 3c525fa7eb5f14ae6c97603acd90aedd316a5acd
 <<<<<<< a2c121f5dd80ae0902d243d64597db69597f670e
 <<<<<<< f6aeb16d50f1c67f1049144afc1c0ff2242ee31e
 
@@ -15,14 +16,23 @@ window.alert("Seja bem vindo(a).");
 	
 =======
 >>>>>>> sem barra de pesquisar
+=======
+
+	
+>>>>>>> sem barra pesquisar
 $(document).ready(function(){
 	
-	$(".fonteitalico").slideUp();
-	$("#flip").click(function(){
-		$("#flip>.fonteitalico").stop().slideToggle("slow");
+	function inicio(){
+		$("#menu").hide();
+		$("#menu").fadeIn("slow");
 	});
+<<<<<<< 3c525fa7eb5f14ae6c97603acd90aedd316a5acd
 <<<<<<< a2c121f5dd80ae0902d243d64597db69597f670e
 >>>>>>> algumas alterações
 =======
 });
 >>>>>>> sem barra de pesquisar
+=======
+
+	inicio();}
+>>>>>>> sem barra pesquisar
