0b929c57932294c131dc469d03aa55de6d52c912
Luísa Antoni da Rocha <luisa.rocha@digitaldesk.com.br>
2016-04-26 16:29:13 -0300

    sem barra de pesquisar

--- luisa/animacao.js
+++ luisa/animacao.js
@@ -1,3 +1,4 @@
+<<<<<<< e4fefd2420f92f7fa75963fe1ba1804963c3a5f9
 
 /*$(document).ready(function(){
 	
@@ -10,9 +11,12 @@
 
 window.alert("Seja bem vindo(a).");*/
 	
+=======
+>>>>>>> sem barra de pesquisar
 $(document).ready(function(){
-		$(".fonteitalico").slideUp();
-		$("#flip").click(function(){
-			$("#flip>.fonteitalico").stop().slideToggle("slow");
-		});
+	
+	$(".fonteitalico").slideUp();
+	$("#flip").click(function(){
+		$("#flip>.fonteitalico").stop().slideToggle("slow");
 	});
+});
