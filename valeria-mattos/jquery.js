33d63bbfdefa9a9ca45aa6e58d9274be5256b743
Valeria De Mattos Rex <valeria.rex@digitaldesk.com.br>
2016-04-27 13:03:09 -0300

    FadeIn

--- valeria-mattos/jquery.js
+++ valeria-mattos/jquery.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
-	 $("#meunome").mouseover(function(){
+	$('.content').hide();
+	$('.content').fadeIn("slow");
+	
+	$("#meunome").mouseover(function(){
         $("#dados").slideUp(800);
     });
     $("#meunome").mouseout(function(){
