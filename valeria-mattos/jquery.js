b23fbb67ab01b5a0e0ddd55f07cff754ac72811b
Valeria De Mattos Rex <valeria.rex@digitaldesk.com.br>
2016-04-27 13:49:50 -0300

    Destaque nas divs

--- valeria-mattos/jquery.js
+++ valeria-mattos/jquery.js
@@ -5,7 +5,35 @@ $(document).ready(function(){
 	$("#meunome").mouseover(function(){
         $("#dados").slideUp(800);
     });
-    $("#meunome").mouseout(function(){
+    $("#meunome").mouseleave(function(){
         $("#dados").slideDown(800);
+    });
+	
+	$(".dadosefoto").mouseover(function(){
+        $(".dadosefoto").animate({borderWidth: "10px"});
+    });
+    $(".dadosefoto").mouseleave(function(){
+        $(".dadosefoto").animate({borderWidth: "3px"});
+    });
+	
+	$(".dadosprofissionais").mouseover(function(){
+        $(".dadosprofissionais").animate({borderWidth: "10px"});
+    });
+    $(".dadosprofissionais").mouseleave(function(){
+        $(".dadosprofissionais").animate({borderWidth: "3px"});
+    });
+	
+	$(".informacoesadicionais").mouseover(function(){
+        $(".informacoesadicionais").animate({borderWidth: "10px"});
+    });
+    $(".informacoesadicionais").mouseleave(function(){
+        $(".informacoesadicionais").animate({borderWidth: "3px"});
+    });
+	
+	$(".contatos").mouseover(function(){
+        $(".contatos").animate({borderWidth: "10px"});
+    });
+    $(".contatos").mouseleave(function(){
+        $(".contatos").animate({borderWidth: "3px"});
     });
 });
