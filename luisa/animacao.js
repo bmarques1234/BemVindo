2f086ed37ec3783b680c90b2dabdbfda3c6b1bd0
Luísa Antoni da Rocha <luisa.rocha@digitaldesk.com.br>
2016-04-27 14:48:13 -0300

    animacao segunda tela

--- luisa/animacao.js
+++ luisa/animacao.js
@@ -1,7 +1,26 @@
-
 $(document).ready(function(){
+		function aparecer(){
+		$('#page').fadeIn("slow");
+		$('#caixa').fadeIn("slow");
+		}
+		
+		function aparecertexto(){
 		$(".fonteitalico").slideUp();
 		$("#flip").click(function(){
 			$("#flip>.fonteitalico").stop().slideToggle("slow");
 		});
-	});
+		
+		$("#desliza").slideUp();
+		$("#aperta").click(function(){
+			$("#aperta>#desliza").stop().slideToggle("slow");
+		});
+		
+		$("#desliza2").slideUp();
+		$("#aperta2").click(function(){
+			$("#aperta2>#desliza2").stop().slideToggle("slow");
+		});
+		}
+		
+		aparecer();
+		aparecertexto();
+});
