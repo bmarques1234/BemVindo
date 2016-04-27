40ef321b08161cfc222ad4ad4a6ad68f7a3fd402
Luísa Antoni da Rocha <luisa.rocha@digitaldesk.com.br>
2016-04-27 11:15:50 -0300

    incluindo animacao certo

--- luisa/animacao.js
+++ luisa/animacao.js
@@ -1,14 +1,4 @@
-/*$(document).ready(function(){
-	function inicio(){
-		$("#menu").hide();
-		$("#menu").fadeIn("slow");
-	});
 
-	inicio();}
-
-window.alert("Seja bem vindo(a).");*/
-	
-	
 $(document).ready(function(){
 		$(".fonteitalico").slideUp();
 		$("#flip").click(function(){
