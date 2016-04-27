a91c981158ee27c0c07c90df6ddf948d1f06db02
Luísa Antoni da Rocha <luisa.rocha@digitaldesk.com.br>
2016-04-27 09:54:54 -0300

    com icone e animacao certo

--- luisa/animacao.js
+++ luisa/animacao.js
@@ -3,13 +3,14 @@
 <<<<<<< f6aeb16d50f1c67f1049144afc1c0ff2242ee31e
 
 	
-$(document).ready(function(){
+/*$(document).ready(function(){
 	
 	function inicio(){
 		$("#menu").hide();
 		$("#menu").fadeIn("slow");
 	});
 
+<<<<<<< 2f5e25977961ccfcee734b35400a1794c9ac9a1e
 	inicio();}
 =======
 window.alert("Seja bem vindo(a).");
@@ -36,3 +37,15 @@ $(document).ready(function(){
 
 	inicio();}
 >>>>>>> sem barra pesquisar
+=======
+	inicio();}*/
+	
+window.alert("Seja bem vindo(a).");
+	
+$(document).ready(function(){
+		$(".fonteitalico").slideUp();
+		$("#flip").click(function(){
+			$("#flip>.fonteitalico").stop().slideToggle("slow");
+		});
+	});
+>>>>>>> com icone e animacao certo
