b5934d5c9cb630bf886e4374ac4ae3c2a7ad4881
Luísa Antoni da Rocha <luisa.rocha@digitaldesk.com.br>
2016-04-27 09:54:54 -0300

    com icone e animacao certo

--- luisa/animacao.js
+++ luisa/animacao.js
@@ -1,10 +1,19 @@
 
 	
-$(document).ready(function(){
+/*$(document).ready(function(){
 	
 	function inicio(){
 		$("#menu").hide();
 		$("#menu").fadeIn("slow");
 	});
 
-	inicio();}
+	inicio();}*/
+	
+window.alert("Seja bem vindo(a).");
+	
+$(document).ready(function(){
+		$(".fonteitalico").slideUp();
+		$("#flip").click(function(){
+			$("#flip>.fonteitalico").stop().slideToggle("slow");
+		});
+	});
