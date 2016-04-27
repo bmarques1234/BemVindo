b1839614f9223b79ee28b06c624b10cc7dda0e33
Luísa Antoni da Rocha <luisa.rocha@digitaldesk.com.br>
2016-04-27 10:29:06 -0300

    consertando mais erros

--- luisa/animacao.js
+++ luisa/animacao.js
@@ -1,18 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< 0f82c95f7a566e6a4fb1163601361897486bfd7f
-<<<<<<< 7af8463083d035c16336a7d35a787e5c41d0962f
-<<<<<<< 010f60a196fbe26e9d446375b8e46a9db73465f3
-<<<<<<< dd404378605b49a16050516fc99b4a8b5823c918
-<<<<<<< 8a8e5c067ecf8275337f89668af207939c537f4f
-<<<<<<< 1126eadf80afe38499b31b1fbf941b9705aad711
-<<<<<<< 0b929c57932294c131dc469d03aa55de6d52c912
-<<<<<<< e4fefd2420f92f7fa75963fe1ba1804963c3a5f9
-
 /*$(document).ready(function(){
-	
-<<<<<<< 7c77a5d82511655b4e8ea282322eba1c51e6b33a
-<<<<<<< 074b5c7045771101785d787612c3a78660c7ea43
-<<<<<<< 0e3c3da057ae52b6ecb330ad85dd1a4dd98e64c2
 	function inicio(){
 		$("#menu").hide();
 		$("#menu").fadeIn("slow");
@@ -22,94 +8,6 @@
 
 window.alert("Seja bem vindo(a).");*/
 	
-=======
->>>>>>> sem barra de pesquisar
-=======
-
-	
->>>>>>> sem barra pesquisar
-$(document).ready(function(){
-=======
-/*$(document).ready(function(){
->>>>>>> com icone e animacao certo
-=======
-
-	
-/*$(document).ready(function(){
->>>>>>> 61f2a63944e61d4af6b2276f0b4c58845fb14236
-	
-	function inicio(){
-		$("#menu").hide();
-		$("#menu").fadeIn("slow");
-	});
-
-<<<<<<< HEAD
-	inicio();}*/
-	
-=======
->>>>>>> algumas alterações
-window.alert("Seja bem vindo(a).");
-	
-=======
->>>>>>> sem barra de pesquisar
-=======
-
-	
->>>>>>> sem barra pesquisar
-$(document).ready(function(){
-=======
-/*$(document).ready(function(){
->>>>>>> com icone mudado
-	
-	function inicio(){
-		$("#menu").hide();
-		$("#menu").fadeIn("slow");
-	});
-
-	inicio();}
-*/
-/*window.alert("Seja bem vindo(a).");*/
-	
-=======
-window.alert("Seja bem vindo(a).");
-	
->>>>>>> algumas alterações
-=======
->>>>>>> sem barra de pesquisar
-=======
-
-	
->>>>>>> sem barra pesquisar
-$(document).ready(function(){
-=======
-/*$(document).ready(function(){
->>>>>>> com icone e animacao certo
-	
-	function inicio(){
-		$("#menu").hide();
-		$("#menu").fadeIn("slow");
-	});
-
-	inicio();}*/
-	
-window.alert("Seja bem vindo(a).");
-=======
-<<<<<<< HEAD
-	inicio();}*/
-	
-window.alert("Seja bem vindo(a).");
-=======
-<<<<<<< HEAD
-	inicio();}
-*/
-/*window.alert("Seja bem vindo(a).");*/
-=======
-	inicio();}*/
-	
-window.alert("Seja bem vindo(a).");
->>>>>>> b5934d5c9cb630bf886e4374ac4ae3c2a7ad4881
->>>>>>> d79dc9f2958d0fb704fb8fe3118c4f5b478aeb26
->>>>>>> 61f2a63944e61d4af6b2276f0b4c58845fb14236
 	
 $(document).ready(function(){
 		$(".fonteitalico").slideUp();
