9b9c78b58fe7aaadfc4127dfb763aa5c8565727b
unknown <marcelo.bohn@N014.digitaldesk.com.br>
2016-04-28 11:14:39 -0300

    organização das pastas

---
+++ dist/js/geison-JS.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+	$('#header').hide();
+	$('#header').css('margin-top', '-5px');
+	$('#header>img').hide();
+	$('#infoPessoais').hide();
+	$('#infoCont').hide();
+	$('#infoProf').hide();
+	$('#infoAdic').hide();
+	$('#back').hide();
+	$('.content-lists > div').hide();
+	
+	$('#header').slideDown(300,function(){
+		$('#header>img').fadeIn(800, function(){
+			$('#infoPessoais').slideDown(300, function(){
+				$('#infoCont').slideDown(300, function(){
+					$('#infoProf').slideDown(300, function(){
+						$('#infoAdic').slideDown(300, function(){
+							$('.content-lists > div').fadeIn(500);
+							$('#back').slideDown(300);
+						});
+					});
+				});
+			});
+		});
+	});
+	
+	setInterval(function(){
+		$('#header>img').delay(1000).animate({opacity:0.5},1500);
+		$('#header>img').delay(500).animate({opacity:1},1500);
+	},1500);
+});
---
+++ update.sh
@@ -0,0 +1,5 @@
+git fetch upstream
+
+git checkout master
+
+git rebase upstream/master
