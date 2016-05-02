6e504739ce825f3d0f31840a8ae493e80c1d8604
Marcelo Bohn <marcelo.bohn@digitaldesk.com.br>
2016-05-02 14:44:17 -0300

    webserver with server name

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
  
+var server = {
+  host: 'localhost', //192.168.1.195
+  port: '8001'
+}
+
 gulp.task('webserver', function() {
   gulp.src('dist')
     .pipe(webserver({
-      fallback: 'dist/index.html'
+      fallback: 'dist/index.html',
+      host: server.host,
+      port: server.port
     }));
 });
 
