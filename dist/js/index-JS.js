a25ced170052c4465ab46a3e2f612946997e280f
Geison Camara <geison.camara@digitaldesk.com.br>
2016-05-02 14:17:31 -0300

    Refatoração JS página index

--- dist/js/index-JS.js
+++ dist/js/index-JS.js
@@ -1,77 +1,48 @@
-$(document).ready(function(){
-	function subir(){
-		$('#imagem > div.mensagem').css('display', 'block');
-		$('#imagem').mouseover(function(){
-			$('#imagem > div.mensagem').stop().animate({top: '90%'}),100;
-		});
-	}
-
-	function descer(){
-		$('#imagem').mouseout(function(){
-			$('#imagem > div.mensagem').stop().animate({top: '100%'}),100;
-		});
-	}
+$(document).ready(function(){	
 	
+	$('#imagem > *').click(function(){
+		var nome = $(this).attr('id');
+		if(nome!=undefined){
+			$(window.document.location).attr('href','perfil/'+nome);
+		}
+	});
+
+	$('#imagem > *').mouseover(function(){
+		var msg = 'Clique aqui para mais informações sobre ';
+		var nome = $(this).attr('id');
+		if(nome!=undefined){
+			var nbsp = nome.replace('-',' ')
+			var firstLetter = nbsp.substring(0,1).toUpperCase();
+			var letters = nbsp.substring(1,20);
+			var positionNbsp = nbsp.indexOf(' ');
+			if(positionNbsp>1){
+				letters = nbsp.substring(1,positionNbsp);
+				var sobrenome = nbsp.substring(positionNbsp,20);
+				var sobFirstLetter = sobrenome.substring(1,2).toUpperCase();
+				var sobLetters = sobrenome.substring(2,20);
+				$('#imagem > .mensagem').html(msg+firstLetter+letters+' '+sobFirstLetter+sobLetters+".");
+			}else{
+				$('#imagem > .mensagem').html(msg+firstLetter+letters+".");
+			}
+			$(this).mouseout(function(){
+				$('#imagem > .mensagem').html("Clique sobre os rostos para mais informações:");
+			});
+		}
+	});
+
 	subir();
 	descer();
-	
-	$('.luisa').click(function(){
-		$(window.document.location).attr('href','perfil/luisa/index.html');
-	});
-	$('.luisa').mouseover(function(){
-		$('#imagem > .mensagem').html("Clique aqui para mais informações sobre Luisa.");
-	});
-	$('.luisa').mouseout(function(){
-		$('#imagem > .mensagem').html("Clique sobre os rostos para mais informações:");
-	});
-	
-	$('.geison').click(function(){
-		$(window.document.location).attr('href','perfil/geison/index.html');
-	});
-	$('.geison').mouseover(function(){
-		$('#imagem > .mensagem').html("Clique aqui para mais informações sobre Geison.");
-	});
-	$('.geison').mouseout(function(){
-		$('#imagem > .mensagem').html("Clique sobre os rostos para mais informações:");
-	});
-	
-	$('.guinter').click(function(){
-		$(window.document.location).attr('href','perfil/guinter/index.html');
-	});
-	$('.guinter').mouseover(function(){
-		$('#imagem > .mensagem').html("Clique aqui para mais informações sobre Guinter.");
-	});
-	$('.guinter').mouseout(function(){
-		$('#imagem > .mensagem').html("Clique sobre os rostos para mais informações:");
-	});
-	
-	$('.valeria').click(function(){
-		$(window.document.location).attr('href','perfil/valeria-mattos/index.html');
-	});
-	$('.valeria').mouseover(function(){
-		$('#imagem > .mensagem').html("Clique aqui para mais informações sobre Valéria.");
-	});
-	$('.valeria').mouseout(function(){
-		$('#imagem > .mensagem').html("Clique sobre os rostos para mais informações:");
-	});
-	
-	$('.brunomarques').click(function(){
-		$(window.document.location).attr('href','perfil/bruno-marques/index.html');
-	});
-	$('.brunomarques').mouseover(function(){
-		$('#imagem > .mensagem').html("Clique aqui para mais informações sobre Bruno Marques.");
-	});
-	$('.brunomarques').mouseout(function(){
-		$('#imagem > .mensagem').html("Clique sobre os rostos para mais informações:");
-	});
-	
-	$('.brunoklein').click(function(){
-		$(window.document.location).attr('href','perfil/bruno-klein/index.html');
-	});
-	$('.brunoklein').mouseover(function(){
-		$('#imagem > .mensagem').html("Clique aqui para mais informações sobre Bruno Klein.");
-	});
-	$('.brunoklein').mouseout(function(){
-		$('#imagem > .mensagem').html("Clique sobre os rostos para mais informações:");
-	});
 });
+
+function subir(){
+	$('#imagem > div.mensagem').css('display', 'block');
+	$('#imagem').mouseover(function(){
+		$('#imagem > div.mensagem').stop().animate({top: '90%'}),100;
+	});
+}
+
+function descer(){
+	$('#imagem').mouseout(function(){
+		$('#imagem > div.mensagem').stop().animate({top: '100%'}),100;
+	});
+}
